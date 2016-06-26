ddfc855e3391d05f3871a099d8eeb93117ab20db
netgrind <cale.bradbury@gmail.com>
2016-06-26 14:45:48 -0300

    initial camp cult pack commit

---
+++ famijam/Assets/CampCult/Scripts/VJ/ccAudioController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+
+public class ccAudioController : MonoBehaviour {
+
+	public static float value = 0;
+	public bool oscMode = true;
+
+	public string oscHost = "127.0.0.1";
+	public int SendToPort = 12000;
+	public int ListenerPort = 32000;
+	public string address = "/vj";
+    float max = 1;
+	
+	public AudioSource source;
+	public int numSamples;
+	public int channel = 0;
+	public FFTWindow FFTWindow;
+	public float lerp = .5f;
+	public AnimationCurve spectrumCurve;
+	public float spectrumMul = 1;
+    public float spectrumPow = 2;
+    public float falloffRate = .01f;
+
+	UDPPacketIO udp;
+	Osc handler;
+    public static float[] FFT = new float[1];
+    static float[] temp = new float[1];
+
+    void OnEnable(){
+		if (oscMode) {
+			udp = gameObject.AddComponent<UDPPacketIO> ();
+			handler = gameObject.AddComponent<Osc> ();
+
+			udp.init (oscHost, SendToPort, ListenerPort);
+			handler.init (udp);
+			handler.SetAddressHandler (address, vjValue);
+			FFT = new float[1];
+            temp = new float[1];
+		} else
+        {
+            FFT = new float[numSamples];
+        }
+	}
+
+	void Update(){
+		if (!oscMode) {
+			float[] n = new float[FFT.Length];
+			source.GetSpectrumData (n, channel, FFTWindow);
+			for(int i = 0; i< FFT.Length;i++){
+				FFT[i] = Mathf.Lerp(FFT[i],spectrumCurve.Evaluate((float)i/FFT.Length)*(n[i]) *spectrumMul,lerp);
+			}
+		}
+	}
+
+	void vjValue(OscMessage msg){
+
+		if (FFT.Length != msg.Values.Count)
+        {
+            FFT = new float[msg.Values.Count];
+            temp = new float[msg.Values.Count];
+            for (int i = 0; i<FFT.Length;i++){
+				FFT[i] = 0;
+                temp[i] = 0;
+			}
+		}
+        float f = 0;
+        max = Mathf.Max(10, max-falloffRate);
+        for (int i = 0; i<FFT.Length;i++){
+            f = spectrumCurve.Evaluate((float)i / FFT.Length)*(float)msg.Values[i];
+            max = Mathf.Max(f, max);
+            temp[i] = f;
+        }
+
+        for (int i = 0; i < FFT.Length; i++)
+        {
+            FFT[i] = Mathf.Lerp(FFT[i], Mathf.Pow((temp[i] / max) * spectrumMul, spectrumPow), lerp);
+        }
+    }
+
+
+	
+}
---
+++ famijam/Assets/CampCult/Shaders/PostFX/Displacement.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+[ExecuteInEditMode]
+[AddComponentMenu("Image Effects/Camp Cult/Displacement/Displacement")]
+public class Displacement : ImageEffectBase {
+
+	public enum MergeType{
+		Lerp,
+		Add,
+		Mul, 
+        Wtf,
+        ColorDodge,
+        LighterColor,
+        VividLight,
+        HardMix,
+        Difference,
+        Subtract,
+        Divide
+    }
+	public MergeType merge;
+    MergeType _merge;
+    string[] enums;
+
+	public Texture lastFrame;
+	public CCTexture flow;
+	public Vector2 offset;
+	public float fade = .9f;
+	public bool radial = true;
+	public float angle = 0;
+	public float anglePerSecond = 0;
+	public Transform center;
+	public bool invertY = true;
+
+    void OnEnable()
+    {
+        enums = System.Enum.GetNames(typeof(MergeType));
+    }
+
+	// Called by camera to apply image effect
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+		angle += anglePerSecond * Time.deltaTime * Mathf.PI * 2;
+
+		flow.Update ();
+
+		Vector3 p = new Vector3 (center.position.x, center.position.y, center.position.z);
+		p = GetComponent<Camera> ().WorldToViewportPoint (p);
+		material.SetVector ("_center", new Vector4 (p.x, p.y, p.z, 0.0f));
+
+		material.SetVector ("_x", new Vector4 (offset.x, offset.y, angle, fade));
+		material.SetTexture ("_Last", lastFrame);
+		material.SetTexture ("_Flow", flow.texture);
+		material.SetVector ("_Flow_ST", flow.scaleTranslate);
+		
+		if (radial) {
+			Shader.EnableKeyword ("radial");
+			Shader.DisableKeyword ("nradial");
+		} else {
+			Shader.EnableKeyword ("nradial");
+			Shader.DisableKeyword ("radial");
+		}
+		if (invertY) {
+			Shader.EnableKeyword ("invert");
+			Shader.DisableKeyword ("ninvert");
+		} else {
+			Shader.EnableKeyword ("ninvert");
+			Shader.DisableKeyword ("invert");
+		}
+		
+        if(_merge != merge)
+        {
+            _merge = merge;
+            foreach (string s in enums)
+            {
+                material.DisableKeyword(s);
+            }
+            material.EnableKeyword(merge.ToString());            
+        }
+
+		
+
+		Graphics.Blit (source, destination, material);
+	}
+}
